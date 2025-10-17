b08483d37edfee11887ab38da85cbef6d4016dbf
BagusMIftahVisaIndonesia <bagus@visa-indonesia.com>
2025-10-17 15:17:51 +0800

    style: adjust container width from max-w-7xl to max-w-11/12

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import ExpenseItem from "@/components/ExpenseItem";
 export default function Home() {
   return (
     <div className="min-h-screen bg-gray-50">
-      <div className="max-w-7xl mx-auto p-4 sm:p-6">
+      <div className=" max-w-11/12 mx-auto p-4 sm:p-6">
         <ReportHeader />
         <ExpenseItem />
       </div>
